1f23f94094b35a89ad0c2f6da92ccb3ebf4befe6
YuukiRen <reinaldoalvin1@gmail.com>
2019-02-27 13:04:03 +0700

    first

---
+++ resources/assets/js/components/Add.vue
@@ -0,0 +1,36 @@
+<template>
+	<div class="modal" :class='openmodal'>
+	  <div class="modal-background"></div>
+	  <div class="modal-card">
+	    <header class="modal-card-head">
+	      <p class="modal-card-title">Modal title</p>
+	      <button class="delete" aria-label="close" @click="close"></button>
+	    </header>
+	    <section class="modal-card-body">
+	      <!-- Content ... -->
+				<div class="field">
+					<label class="label">Name</label>
+					<div class="control">
+						<input class="input" type="text" placeholder="Text input">
+					</div>
+				</div>
+				<!-- Content ... -->
+	    </section>
+	    <footer class="modal-card-foot">
+	      <button class="button is-success">Save changes</button>
+	      <button class="button">Cancel</button>
+	    </footer>
+	  </div>
+	</div>
+</template>
+
+<script>
+	export default{
+		props:['openmodal'],
+		methods:{
+			close(){
+				this.$emit('closeRequest')
+			}
+		}
+	}
+</script>
